662d1a470a9239657b8795d999f6b8db3e9d7dfb
khanhtrinh301103 <s3927152@rmit.edu.vn>
2024-05-17 00:07:56 +0700

    finish for task 2 but not task 3

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
+# from flask import Flask, render_template, request, url_for
+# from werkzeug.utils import secure_filename
+# import os
+# import shutil
+# from image_processing import extract_features, find_closest_category, recommend_similar_images, compute_average_features, extract_features_from_category
+# from tensorflow.keras.applications import EfficientNetB0
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = 'static/uploads'
+# os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+# model = EfficientNetB0(weights='imagenet', include_top=False, pooling='max')
+# category_features = compute_average_features('dataset/test', model)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file:
+#             filename = secure_filename(file.filename)
+#             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             file.save(file_path)
+#             category, style = find_closest_category(file_path, category_features, model).split('_')
+#             features = extract_features(file_path, model)
+#             image_paths, features_list = extract_features_from_category('dataset/test', f"{category}_{style}", model)
+#             recommended_images, similarity_scores = recommend_similar_images(features, features_list, image_paths)
+
+#             recommended_image_paths = []
+#             for img_path in recommended_images:
+#                 dest_path = os.path.join(app.config['UPLOAD_FOLDER'], os.path.basename(img_path))
+#                 shutil.copy(img_path, dest_path)
+#                 recommended_image_paths.append(os.path.basename(dest_path))
+
+#             uploaded_image_url = os.path.basename(file_path)
+#             recommendations = list(zip(recommended_image_paths, similarity_scores))
+#             return render_template('index.html', uploaded_image=uploaded_image_url, recommendations=recommendations, category=category, style=style)
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# task 2============================================================================================
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import secure_filename
 import os
 import shutil
-from image_processing import extract_features, find_closest_category, recommend_similar_images, compute_average_features, extract_features_from_category
+from image_processing import extract_features, extract_features_from_dataset, recommend_similar_images
 from tensorflow.keras.applications import EfficientNetB0
 
 app = Flask(__name__)
@@ -10,7 +52,7 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 model = EfficientNetB0(weights='imagenet', include_top=False, pooling='max')
-category_features = compute_average_features('dataset/test', model)
+image_paths, features_list = extract_features_from_dataset('dataset/test', model)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -20,9 +62,7 @@ def home():
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            category, style = find_closest_category(file_path, category_features, model).split('_')
             features = extract_features(file_path, model)
-            image_paths, features_list = extract_features_from_category('dataset/test', f"{category}_{style}", model)
             recommended_images, similarity_scores = recommend_similar_images(features, features_list, image_paths)
 
             recommended_image_paths = []
@@ -33,7 +73,7 @@ def home():
 
             uploaded_image_url = os.path.basename(file_path)
             recommendations = list(zip(recommended_image_paths, similarity_scores))
-            return render_template('index.html', uploaded_image=uploaded_image_url, recommendations=recommendations, category=category, style=style)
+            return render_template('index.html', uploaded_image=uploaded_image_url, recommendations=recommendations)
     return render_template('index.html')
 
 if __name__ == '__main__':
